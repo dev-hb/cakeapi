dc303021f2a9bbd3f12f151ca782a28fb1b41fdf
ShadMaN <zakaria.hba.97@gmail.com>
2020-03-11 11:07:46 +0100

    Added updater.bat

--- vendor/updater.bat
+++ vendor/updater.bat
@@ -2,15 +2,17 @@ echo off
 
 echo Downloading API Provider ... please wait
 
-git clone https://github.com/dev-hb/apiprovider.git temp
+rmdir tmp
+
+git clone https://github.com/dev-hb/apiprovider.git tmp
 
 echo Updating API Provider ...
 
 rem move the files
-for %%i in (temp\*) do move "%%i" .
+for %%i in (tmp\*) do move "%%i" .
 rem move the directories
-for /d %%i in (temp\*) do move "%%i" .
+for /d %%i in (tmp\*) do move "%%i" .
 
-rmdir temp
+rmdir tmp /S /Q
 
 echo Finished!
