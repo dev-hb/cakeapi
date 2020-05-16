7b12e67da79cc9cf4f3c53c9a679f7455d177341
ShadMaN <zakaria.hba.97@gmail.com>
2020-05-16 21:06:28 +0000

    Updated updater.bat

--- updater.bat
+++ updater.bat
@@ -8,11 +8,4 @@ git clone https://github.com/dev-hb/apiprovider.git tmp
 
 echo Updating API Provider ...
 
-rem move the files
-for %%i in (tmp\*) do move "%%i" .
-rem move the directories
-for /d %%i in (tmp\*) do move "%%i" .
-
-rmdir tmp /S /Q
-
 echo Finished!
